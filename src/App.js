45384e9ccba3c8a2539d4d530df552e24838e811
Christoph Obst <christoph.obst@posteo.de>
2023-03-14 12:10:21 +0100

    add-checkbox

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import EntryForm from "./components/EntryForm";
 
 // Structure: WeatherDisplay, WeatherIcon, EntriesSection, Entries, EntryForm, GoodWeatherChechbox, Button
 
 function App() {
-  return;
+
+  return(
+    <main className="app__main">
+      <EntryForm /> 
+     </main>
+  )
 }
 
 export default App;
--- src/components/EntryForm/index.js
+++ src/components/EntryForm/index.js
@@ -1,15 +1,26 @@
 export default function EntryForm() {
 
 
-
     return(
-<form className="entry-form" onSubmit={}>
-    <h2>Add new Activiity</h2>
+<form className="entry-form" onSubmit="">
+    <h2>Add new Activity</h2>
 
         <div className="entry-form__field">
-            <label htmlFor="activity">Name</label>
-            <input type="text" name="activity" id="activity"></input>
+            <label htmlFor="activity">Name:</label>
+            <input type="text" name="activity" id="activity"></input>            
         </div>
+
+
+        <div className="entry-form__field">
+            <label htmlFor="goodWeather">Good-weather activity:</label>
+            <input type="checkbox" name="goodWeather" id="goodWeather"></input>            
+        </div>
+
+        <div className="entry-form__button-wrapper">
+        {/*  <Button type="submit">Submit</Button> */}
+        </div>
+
+
 
 </form>
 
